cdaf28ae544f00bd308a40730bdba0ef30eb5930
moutella <joaogabrielmsc@gmail.com>
2021-09-03 17:48:08 -0300

    fix(levels): level 2 and 3 weren't working properly

--- che/cheL3.js
+++ che/cheL3.js
@@ -20,17 +20,21 @@ export default class CheL3 {
   getCurveHalfEdge(vertexId) {
     return this._tableVertexHalfEdge[vertexId];
   }
-
+  setSpecialOpposite(heId, nCurve) {
+    this._che.level1.setOpposite(heId, -nCurve)
+  }
   computeCurveHalfEdge() {
     let visited = new Array(this._che.halfEdgeCount).fill(false);
+
+
 
     for (let heId = 0; heId < this._che.halfEdgeCount; heId++) {
       if (this._che.getOppositeHalfEdge(heId) == -1 && !visited[heId]) {
         this._tableCurveHalfEdge.push(heId);
         this._nCurves++
+        this.setSpecialOpposite(heId, this._nCurves)
         let heIdVisited = heId;
         do {
-
           visited[heIdVisited] = true;
           while (this._che.getOppositeHalfEdge(this._che.nextHalfEdge(heIdVisited)) >= 0) {
             heIdVisited = this._che.getOppositeHalfEdge(this._che.nextHalfEdge(heIdVisited))
@@ -38,7 +42,6 @@ export default class CheL3 {
           heIdVisited = this._che.nextHalfEdge(heIdVisited)
         }
         while (heId != heIdVisited)
-        break
       }
     }
 
